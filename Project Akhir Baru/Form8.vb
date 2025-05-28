18522be9064485579197f27bd6029e6fc33fb35d
sharon <desharonrembet@gmail.com>
2025-05-28 20:09:43 +0700

    pesanan tersimpan

--- Project Akhir Baru/Form8.vb
+++ Project Akhir Baru/Form8.vb
@@ -5,6 +5,11 @@ Public Class Form8
     Private Sub Form8_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         TampilDataPaket()
         TampilDataTambahan()
+        Dim total_paket As Integer = lblHargaTotalPaket.Text
+        Dim total_tambahan As Integer = lblTotalHargaTambahan.Text
+        Dim hasil = total_paket + total_tambahan
+        'Dim hasil = lblHargaTotalPaket.Text + lblTotalHargaTambahan.Text
+        lblTotalHargaSemua.Text = Convert.ToInt32(hasil).ToString("N0")
     End Sub
     Public Sub TampilDataPaket()
         Dim i
@@ -14,12 +19,13 @@ Public Class Form8
         myDataReader = myCommand.ExecuteReader
         If myDataReader.HasRows Then
             While myDataReader.Read()
+                lblNamaPaket.Text = myDataReader("nama_paket")
                 DataGridViewPaket.Rows.Add()
                 DataGridViewPaket.Item(0, i).Value = myDataReader("sub_isi_paket")
                 DataGridViewPaket.Item(1, i).Value = myDataReader("detail_sub_paket")
                 DataGridViewPaket.Item(2, i).Value = "1"
                 DataGridViewPaket.Item(3, i).Value = myDataReader("id_paket")
-                'lblTotalHargaPaket.Text = myDataReader("harga_paket")
+                lblHargaTotalPaket.Text = Convert.ToInt32(myDataReader("harga_paket")).ToString("N0")
                 i = i + 1
             End While
             myDataReader.Close()
@@ -40,24 +46,29 @@ Public Class Form8
                 Dim harga As Integer = myDataReader("harga_paket")
                 Dim jumlah As Integer = myDataReader("jumlah_paket")
                 DataGridViewTambahan.Rows.Add()
-                DataGridViewTambahan.Item(0, i).Value = myDataReader("nama_paket")
-                DataGridViewTambahan.Item(1, i).Value = myDataReader("nama_paket")
+                DataGridViewTambahan.Item(0, i).Value = myDataReader("sub_isi_paket")
+                DataGridViewTambahan.Item(1, i).Value = myDataReader("detail_sub_paket")
                 DataGridViewTambahan.Item(2, i).Value = jumlah
-                DataGridViewTambahan.Item(3, i).Value = myDataReader("id_paket")
+                DataGridViewTambahan.Item(3, i).Value = jumlah * harga
+                DataGridViewTambahan.Item(4, i).Value = myDataReader("id_paket")
                 i = i + 1
             End While
-            'Dim totalPengeluaran As Integer = 0
-            'For Each row As DataGridViewRow In DataGridViewPaket.Rows
-            '    If Not row.IsNewRow AndAlso Not IsDBNull(row.Cells("colTotalTambahan").Value) Then
-            '        totalPengeluaran += Convert.ToInt32(row.Cells("colTotalTambahan").Value)
-            '    End If
-            'Next
+            Dim totalPengeluaran As Integer = 0
+            For Each row As DataGridViewRow In DataGridViewTambahan.Rows
+                If Not row.IsNewRow AndAlso Not IsDBNull(row.Cells("colTotalTambahan").Value) Then
+                    totalPengeluaran += Convert.ToInt32(row.Cells("colTotalTambahan").Value)
+                End If
+            Next
 
-            'lblTotalHargaTambahan.Text = totalPengeluaran.ToString("N0")
+            lblTotalHargaTambahan.Text = totalPengeluaran.ToString("N0")
             myDataReader.Close()
         End If
         If myDataReader.IsClosed = False Then
             myDataReader.Close()
         End If
+    End Sub
+
+    Private Sub btnHome_Click(sender As Object, e As EventArgs) Handles btnHome.Click
+        Me.Hide()
     End Sub
 End Class
